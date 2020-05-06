1dc4548e6aaacce6d968e7c96a7b8bea5234e1ab
mina.h <mp922352612@gmail.com>
2020-05-06 18:29:22 +0800

    Init project

---
+++ features/annotations/variables.ts
@@ -0,0 +1,31 @@
+let apples = 5;
+let speed = 'fast';
+let hasName = true;
+
+let nothingMuch = null;
+let nothing = undefined;
+
+// built in objects
+let now = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+	x: 10,
+	y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+	console.log(i);
+};
