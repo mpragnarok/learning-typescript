b8c957a612e9a85d885457e19bbbeae38cedb2a8
mina.h <mp922352612@gmail.com>
2020-05-07 13:45:40 +0800

    Annotations for anonymous functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -3,5 +3,14 @@ const add = (a: number, b: number) => {
 };
 
 const subtract = (a: number, b: number): number => {
-	a - b;
+	return a - b;
+};
+
+function divide(a: number, b: number): number {
+	return a / b;
+}
+
+// Annotations for anonymous functions
+const multiply = function (a: number, b: number): number {
+	return a * b;
 };
