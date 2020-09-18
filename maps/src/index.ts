029499cdbe8daa76c38005f6229323fd589e8f21
mina.h <mp922352612@gmail.com>
2020-09-18 14:12:10 +0800

    <add><CustomMap to addMarker>

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,18 +1,11 @@
-// import { User } from "./User";
-// import { Company } from "./Company";
+// install @types/googlemaps to let TS understand google global vars
 import "googlemaps";
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+import { Company } from "./Company";
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
 
-// // install @types/googlemaps to let TS understand google global vars
-// const user = new User();
-// console.log(user);
-// // never use export default in TS
-// // import color from "./User";
-// // console.log(color);
-
-// const company = new Company();
-// console.log(company);
-
-new google.maps.Map(document.getElementById("map"), {
-    zoom: 1,
-    center: { lat: 0, lng: 0 },
-});
+customMap.addMarker(user);
+customMap.addMarker(company);
