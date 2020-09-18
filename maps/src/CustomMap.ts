7c52b4c97f0a2c6ee3f2bb735c0a2449a31e8c77
mina.h <mp922352612@gmail.com>
2020-09-18 14:27:04 +0800

    <add><markerContent method>

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -5,6 +5,7 @@ interface Mappable {
         lat: number;
         lng: number;
     };
+    markerContent(): string;
 }
 
 export class CustomMap {
@@ -18,12 +19,18 @@ export class CustomMap {
     }
     // | or operator will give you the common property between User and Company, location in this case
     addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng,
             },
+        });
+        marker.addListener("click", () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent(),
+            });
+            infoWindow.open(this.googleMap, marker);
         });
     }
 }
