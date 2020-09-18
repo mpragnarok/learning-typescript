128c0615ac65f24ab2b17e0fd0df5fa3ee9f35af
mina.h <mp922352612@gmail.com>
2020-09-18 11:58:19 +0800

    <add><google maps integration>

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,18 @@
-import { User } from "./User";
-import { Company } from "./Company";
-const user = new User();
-console.log(user);
-// never use export default in TS
-// import color from "./User";
-// console.log(color);
+// import { User } from "./User";
+// import { Company } from "./Company";
+import "googlemaps";
 
-const company = new Company();
-console.log(company);
+// // install @types/googlemaps to let TS understand google global vars
+// const user = new User();
+// console.log(user);
+// // never use export default in TS
+// // import color from "./User";
+// // console.log(color);
+
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: { lat: 0, lng: 0 },
+});
