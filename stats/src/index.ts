78cd55e06c8819662aba402272aacd7c04621685
mpragnarok <mp922352612@gmail.com>
2020-09-19 07:57:17 +0800

    <extracting CSV reader>

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,23 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-    .readFileSync("football.csv", {
-        encoding: "utf-8", // change the data to string
-    })
-    .split("\n")
-    .map((row: string): string[] => {
-        return row.split(",");
-    });
-const homeWin = "H";
-const awayWin = "A";
+const reader = new CsvFileReader("football.csv");
+
+reader.read();
+
+// enum - enumeration
+enum MatchResult {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D",
+}
+
+MatchResult.Draw;
+
 let manUnitedWins = 0;
-for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === homeWin) {
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
-    } else if (match[2] === "Man United" && match[5] === awayWin) {
+    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
