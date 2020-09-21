ddab38b3379ba7f8164c0b264ae018dd85d11c12
mina.h <mp922352612@gmail.com>
2020-09-21 17:02:19 +0800

    <refactor><finish user refactoring>

--- web-framework/src/models/Model.ts
+++ web-framework/src/models/Model.ts
@@ -1,8 +1,8 @@
-import { AxiosPromise } from "axios";
+import { AxiosPromise, AxiosResponse } from "axios";
 
 interface ModelAttributes<T> {
     set(update: T): void;
-    getALl(): T;
+    getAll(): T;
     get<K extends keyof T>(key: K): T[K]; // 165. An Advanced Generic Constraint
 }
 
@@ -15,4 +15,52 @@ interface Events {
     on(eventName: string, callback: () => void): void;
     trigger(eventName: string): void;
 }
-export class Model {}
+
+interface HasId {
+    id?: number;
+}
+export class Model<T extends HasId> {
+    constructor(
+        private attributes: ModelAttributes<T>,
+        private events: Events,
+        private sync: Sync<T>
+    ) {}
+
+    get on() {
+        // not calling function, it's returning events.on as a reference
+        return this.events.on;
+    }
+
+    get trigger() {
+        return this.events.trigger;
+    }
+    get get() {
+        return this.attributes.get;
+    }
+
+    set(update: T): void {
+        this.attributes.set(update);
+        this.events.trigger("change");
+    }
+
+    fetch(): void {
+        const id = this.get("id");
+        if (typeof id !== "number") {
+            throw new Error("Cannot fetch without an id");
+        }
+        this.sync.fetch(id).then((response: AxiosResponse): void => {
+            this.set(response.data);
+        });
+    }
+
+    save(): void {
+        this.sync
+            .save(this.attributes.getAll())
+            .then((response: AxiosResponse): void => {
+                this.trigger("save");
+            })
+            .catch(() => {
+                this.trigger("error");
+            });
+    }
+}
--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -1,8 +1,7 @@
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { Model } from "./Model";
 import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
-
+import { ApiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
 export interface UserProps {
     // ? is optional to have
     id?: number;
@@ -11,49 +10,13 @@ export interface UserProps {
 }
 
 const rootUrl = "http://localhost:3000/users";
-export class User {
-    public events: Eventing = new Eventing();
-    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-    public attributes: Attributes<UserProps>;
-    constructor(attrs: UserProps) {
-        this.attributes = new Attributes<UserProps>(attrs);
-    }
 
-    get on() {
-        // not calling function, it's returning events.on as a reference
-        return this.events.on;
-    }
-
-    get trigger() {
-        return this.events.trigger;
-    }
-    get get() {
-        return this.attributes.get;
-    }
-
-    set(update: UserProps): void {
-        this.attributes.set(update);
-        this.events.trigger("change");
-    }
-
-    fetch(): void {
-        const id = this.get("id");
-        if (typeof id !== "number") {
-            throw new Error("Cannot fetch without an id");
-        }
-        this.sync.fetch(id).then((response: AxiosResponse): void => {
-            this.set(response.data);
-        });
-    }
-
-    save(): void {
-        this.sync
-            .save(this.attributes.getAll())
-            .then((response: AxiosResponse): void => {
-                this.trigger("save");
-            })
-            .catch(() => {
-                this.trigger("error");
-            });
+export class User extends Model<UserProps> {
+    static buildUser(attrs: UserProps): User {
+        return new User(
+            new Attributes<UserProps>(attrs),
+            new Eventing(),
+            new ApiSync<UserProps>(rootUrl)
+        );
     }
 }
