d17ba9dd74b64be3a9ea74b879c90397f826bf2c
mina.h <mp922352612@gmail.com>
2020-09-21 14:41:02 +0800

    <refactor><fetching user data>

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "new record", age: 0 });
+const user = new User({ id: 1 });
 
 user.on("change", () => {
-    console.log("user changed");
+    console.log(user);
 });
 
-user.trigger("change");
+user.fetch();
--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosResponse } from "axios";
 
 export interface UserProps {
     // ? is optional to have
@@ -28,5 +29,20 @@ export class User {
     }
     get get() {
         return this.attributes.get;
+    }
+
+    set(update: UserProps): void {
+        this.attributes.set(update);
+        this.events.trigger("change");
+    }
+
+    fetch(): void {
+        const id = this.get("id");
+        if (typeof id !== "number") {
+            throw new Error("Cannot fetch without an id");
+        }
+        this.sync.fetch(id).then((response: AxiosResponse): void => {
+            this.set(response.data);
+        });
     }
 }
