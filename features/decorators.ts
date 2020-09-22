2bbda273ebf80f56b0ed2ed2d03b4b78d1318986
mina.h <mp922352612@gmail.com>
2020-09-22 15:33:05 +0800

    <decorator><more on decorators>

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,3 +1,8 @@
+/*
+We can use decorators on:
+ static methods, accessories and properties */
+
+@classDecorator
 class Boat {
     @testDecorator
     color: string = "red";
@@ -7,15 +12,28 @@ class Boat {
     }
 
     @logError("Opps boat was sunk in ocean")
-    pilot(): void {
-        throw new Error();
-        console.log("swish");
+    pilot(
+        @parameterDecorator speed: string,
+        @parameterDecorator generateWake: boolean
+    ): void {
+        if (speed === "fast") {
+            console.log("swish");
+        } else {
+            console.log("nothing");
+        }
     }
 }
 
+function classDecorator(constructor: typeof Boat) {
+    console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+    console.log(key, index);
+}
 function testDecorator(target: any, key: string) {
     // !target is a prototype of Boat class, prototype in JS only store method's definition, we cannot read the property of prototype
-    console.log(target); // Boat{ pilot: ()=>void}
+    console.log(target); // Boat{ pilot: Function}
     console.log(target.color); // undefined
     console.log(key);
 }
