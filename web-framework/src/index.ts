745c59931762c8869c69d29459666d6511dee70b
mina.h <mp922352612@gmail.com>
2020-09-22 14:40:48 +0800

    <complete><web-framework><CollectionView Implementation>

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,13 +1,19 @@
-import { UserEdit } from "./views/UserEdit";
-import { User } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const user = User.buildUser({ name: "Name", age: 20 });
+const users = new Collection(
+    "http://localhost:3000/users",
+    (json: UserProps) => {
+        return User.buildUser(json);
+    }
+);
+console.log("users", users);
+users.on("change", () => {
+    const root = document.getElementById("root");
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
 
-const root = document.getElementById("root");
-if (root) {
-    const userEdit = new UserEdit(root, user);
-    userEdit.render();
-    console.log(userEdit);
-} else {
-    throw new Error("Root element not found");
-}
+users.fetch();
